5206e722e0b5e2bec00cbaabb7ab4319404ac40a
Kratik00 <159747619+Kratik00@users.noreply.github.com>
2024-12-23 21:47:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    api_id = "22475741"
-api_hash = "1a217be71a0225e0a678af286c211f8a"
-bot_token = "7347353498:AAGgNzcVVGcEUoZSYAhghggmKpgX8_XUYvs
-    AUTH_USERS = "6562642609"
+    API_ID = "25318125"
+    API_HASH = "1a217be71a0225e0a678af286c211f8a"
+    BOT_TOKEN = "b29fb6a928e8b8a3308f8c2d3ba9cfb0"
+    AUTH_USERS = "7376514183"
  
